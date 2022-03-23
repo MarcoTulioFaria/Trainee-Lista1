4dd3fb89ad7f8b443258cb5ed9707117ab04be20
MarcoTulioFaria <mtuliofaria@hotmail.com>
2022-03-23 15:45:27 -0300

    Correção exercicios 1 e 2

--- ex2.rb
+++ ex2.rb
@@ -1,22 +1,18 @@
+
+
 n = gets.to_i
-is_primo = true
 
-def primo?(n)
-    for i in 2..n-1
-        if (n % i == 0)
-            is_primo = false
-            return n - 1
-        end
-    end
-end 
+def IsPrimo(n)
+    (2..n - 1).any? {|i| n % i == 0}
+end
 
-if (n < 2)
+if n < 2
     puts("Não foi possível encontrar um número primo")
 else
-    number = n
-    primo?(number)
-    while is_primo == false
-        primo?(number)
+    while IsPrimo(n)
+        n -= 1
     end
-    puts("#{number}")
+    puts n
 end
+
+         
